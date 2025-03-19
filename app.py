8e498fd4ac6651faa9914436006356b200064319
likithsshetty <likithsuresh22@gmail.com>
2025-03-19 11:48:06 +0530

    altered ports

--- app.py
+++ app.py
@@ -52,8 +52,7 @@ def predict():
                 "message": "Still want to continue?",
             })
 
-        # Return response
         return data
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=80)
